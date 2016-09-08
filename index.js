94ab936a78cb53fac15b5e5b3b5b83e09978b18c
dukai <dukai86@gmail.com>
2016-09-08 21:13:57 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var express = require('express');
+"use strict"
+const crypto = require('crypto');
+const Segment = require('segment');
+var segment = new Segment();
+segment.useDefault();
+
+const express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
 var upload = multer(); // for parsing multipart/form-data
 const PORT = 8090;
-
+const Template = require('template');
 const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -13,6 +19,55 @@ const connection = mysql.createConnection({
   database: 'luluo_db'
 });
 connection.connect();
+var fs = require('fs'); // this engine requires the fs module
+const redis = require("redis"),
+    client = redis.createClient();
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+app.engine('html', function (filePath, options, callback) { // define the template engine
+  fs.readFile(filePath, function (err, content) {
+    if (err) return callback(new Error(err));
+    // this is an extremely simple template engine
+    var t = new Template(content.toString());
+    var rendered = t.render(options);
+    return callback(null, rendered);
+  });
+});
+app.set('views', './views'); // specify the views directory
+app.set('view engine', 'html'); // register the template engine
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/search', (req, res) => {
+  var keywords = req.query.s;
+  var result = segment.doSegment(keywords, {
+    stripStopword: true,
+    simple: true,
+    stripPunctuation: true
+  });
+  result = result.map(val => {
+    let md5 = crypto.createHash('md5');
+    md5.update(val);
+    val = md5.digest('hex');
+    return `w:${val}`
+  })
+
+  client.sinter(result, (err, replies) => {
+    connection.query('select * from goods where id in (' + replies.join(',') + ') limit 10', (err, rows) => {
+      res.render('search', {
+        list: rows,
+        s: keywords
+      })
+    });
+  })
+});
+
+
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
